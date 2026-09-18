8f95b9c45ea6ea1594e86b8f0607a382c2c20991
Mohammadagil <agil.rofikul.007@gmail.com>
2026-09-18 21:06:05 +0700

    Initial commit from downloaded source

---
+++ app/Models/Admin/BarangmasukModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangmasukModel extends Model
+{
+    use HasFactory;
+    protected $table = "tbl_barangmasuk";
+    protected $primaryKey = 'bm_id';
+    protected $fillable = [
+        'bm_noref',
+        'bm_kode',
+        'barang_kode',
+        'customer_id',
+        'bm_tanggal',
+        'bm_jumlah',
+    ]; 
+}
